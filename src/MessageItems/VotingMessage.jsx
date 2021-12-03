907b6ed161a031c7d2565d37eeb5467f3648a27e
michellewong27 <michellekwong2@gmail.com>
2021-12-02 19:02:42 -0500

    Dropdown menu renders under btn & moves with screen sizing; Removed dropdown option 'Edit' for VotingMessage

--- src/MessageItems/VotingMessage.jsx
+++ src/MessageItems/VotingMessage.jsx
@@ -371,37 +371,6 @@ export default function VotingMessage(props) {
                       </span>
                     </li>
                   )}
-                  {pressedUpdate && !showForm && (
-                    <li
-                      className="dropdown__menu-item"
-                      onClick={() =>
-                        onUpdateMessage(message.messageId, messageText)
-                      }
-                    >
-                      <span className="dropdown__menu-item-text">Save</span>
-                    </li>
-                  )}
-                  {pressedUpdate && (
-                    <li
-                      className="dropdown__menu-item"
-                      onClick={() => setPressedUpdate(false)}
-                      key={message.messageId}
-                    >
-                      <span className="dropdown__menu-item-text">Cancel</span>
-                    </li>
-                  )}
-
-                  {!pressedUpdate && !showForm && (
-                    <li
-                      className="dropdown__menu-item"
-                      onClick={() => {
-                        setPressedUpdate(true);
-                      }}
-                    >
-                      <span className="dropdown__menu-item-text">Edit</span>
-                    </li>
-                  )}
-
                   {showForm && (
                     <li
                       className="dropdown__menu-item"
