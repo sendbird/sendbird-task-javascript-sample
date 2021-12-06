176cb1765f4e1b168de9fa75e8ef137275117f97
michellewong27 <michellekwong2@gmail.com>
2021-12-06 18:41:10 -0500

    CSS form

--- src/MessageItems/AddSuggestedTask.jsx
+++ src/MessageItems/AddSuggestedTask.jsx
@@ -11,16 +11,18 @@ export default function AddSuggestedTask({
     <div className="bg-modal" style={{'display': 'flex'}}>
       <div className="modal-content">
         <div className="add_suggested_task_close_btn"   onClick={() => setShowForm(false)} >+</div>
+        <h3 id="suggestion-task-form-title">Change Suggestion Task</h3>
         <form onSubmit={(e) => {changeSuggestionSubmit(e)}}>
           <input
             type="text"
+            id="suggestion_form_input"
             placeholder="Suggested Task"
             value={messageText}
             onChange={(event) => {
               changeMessageText(event.target.value);
             }}
           ></input>
-          <button className="add_suggested_task_save_btn">Submit</button>
+          <button id="add_suggested_task_save_btn">Submit</button>
         </form>
       </div>
     </div>
